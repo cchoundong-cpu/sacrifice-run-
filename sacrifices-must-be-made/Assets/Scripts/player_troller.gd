b9f9be6ca8a24d7971e33429b2bfa1337c20ec97
cchoundong-cpu <cchoundong@gmail.com>
2025-09-28 11:31:37 -0400

    Add files via upload
    Theme: Sacrifices must be made.
    Story & Objective: The player’s journey demands giving up valuables, lives, or easier routes to advance. Progress proves that every gain comes with loss.
    Gameplay Link: Collectibles help but can slow you down; safe paths may cost items; victory comes from smart sacrifices.

---
+++ sacrifices-must-be-made/Assets/Scripts/player_troller.gd
@@ -0,0 +1,34 @@
+extends CharacterBody2D
+
+@export var speed = 10.0
+@export var jump_power = 10.0
+
+var speed_multiplier = 30.0
+var jump_multiplier = -30.0
+var direction = 3
+
+#const SPEED = 300.0
+#const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project setting to be synced with RigidBody nodes
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
+		velocity.y = jump_power * jump_multiplier
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	direction = Input.get_axis("Move left", "Move Right")
+	if direction:
+		velocity.x = direction * speed * speed_multiplier
+	else:
+		velocity.x = move_toward(velocity.x, 0,speed * speed_multiplier )
+
+	move_and_slide()
